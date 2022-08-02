4fb5e4d2b835105b84d6b2fa70e69107ed28c39d
0x0elliot <wolfwithahat@protonmail.com>
2022-08-02 15:30:02 +0530

    Adding auth API to urls.py & Fixing pre-commit configs to support isort and removing EsLinting for now

--- backend/Eye/Eye/urls.py
+++ backend/Eye/Eye/urls.py
@@ -14,8 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.ourapp.com/policies/terms/",
+        contact=openapi.Contact(email="yurmom@gmail.com"),
+        license=openapi.License(name="Test License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("auth/", include("authentication.urls")),
 ]
