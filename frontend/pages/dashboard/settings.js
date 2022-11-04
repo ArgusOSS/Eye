c3f60dd58e0b586967baeb3e9da27334db586041
SyedAhkam <smahkam57@gmail.com>
2022-11-05 00:16:00 +0530

    Create settings page

---
+++ frontend/pages/dashboard/settings.js
@@ -0,0 +1,15 @@
+import { BaseDashboardLayout } from "../../src/components/dashboard/Base";
+import { DashboardSettings } from "../../src/components/dashboard/Settings";
+
+const styles = {
+  marginTop: 30,
+  textAlign: "center",
+};
+
+export default function Settings() {
+  return (
+    <BaseDashboardLayout activeLink="Settings">
+      <DashboardSettings />
+    </BaseDashboardLayout>
+  );
+}
