ac2850ec0a1657f7dd4ea4117dc42807810a5d12
SyedAhkam <smahkam57@gmail.com>
2022-11-05 04:16:43 +0530

    Make theme switcher work

--- frontend/src/components/dashboard/Status/index.js
+++ frontend/src/components/dashboard/Status/index.js
@@ -1,9 +1,10 @@
 import styles from "../../../../styles/Home.module.css";
+import { Text } from "@mantine/core";
 
 export function DashboardStatus() {
     return (
         <div className="container">
-            <h1>Status</h1>
+            <Text size="36px" weight={900}>Status</Text>
         </div>
     )
 }
