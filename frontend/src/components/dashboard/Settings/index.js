7939e3c2d2753ab9af662a0dca7b530dc5975c51
SyedAhkam <smahkam57@gmail.com>
2022-11-06 00:51:28 +0530

    Make email login work

--- frontend/src/components/dashboard/Settings/index.js
+++ frontend/src/components/dashboard/Settings/index.js
@@ -12,18 +12,18 @@ export function DashboardSettings() {
 
             <Accordion variant="separated" radius="lg" defaultValue="customization">
                 <Accordion.Item value="customization">
-                    <Accordion.Control>Customization</Accordion.Control>
-                    <Accordion.Panel>Colors, fonts, shadows and many other parts are customizable to fit your design needs</Accordion.Panel>
+                    <Accordion.Control>AWS-Server-1 [ServerID:EUR1-1635-243]</Accordion.Control>
+                    <Accordion.Panel>Server Status: ACTIVE</Accordion.Panel>
                 </Accordion.Item>
 
                 <Accordion.Item value="flexibility">
-                    <Accordion.Control>Flexibility</Accordion.Control>
-                    <Accordion.Panel>Configure components appearance and behavior with vast amount of settings or overwrite any part of component styles</Accordion.Panel>
+                    <Accordion.Control>AWS-Server-2 [ServerID:ASIA1-1634-553]</Accordion.Control>
+                    <Accordion.Panel>Server Status: ACTIVE</Accordion.Panel>
                 </Accordion.Item>
 
                 <Accordion.Item value="focus-ring">
-                    <Accordion.Control>No annoying focus ring</Accordion.Control>
-                    <Accordion.Panel>With new :focus-visible pseudo-class focus ring appears only when user navigates with keyboard</Accordion.Panel>
+                    <Accordion.Control>AZURE-Server-1 [ServerID:343/EUR243/221]</Accordion.Control>
+                    <Accordion.Panel>Server Status: ACTIVE</Accordion.Panel>
                 </Accordion.Item>
             </Accordion>
         </Container>
