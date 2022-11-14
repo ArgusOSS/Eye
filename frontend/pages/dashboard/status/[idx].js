560961e0a0500def031182c03a8ca4ebf06de1ed
Abheek Tripathy <abheek.tripathy@gmail.com>
2022-11-14 15:05:39 +0530

    eslint-fix

--- frontend/pages/dashboard/status/[idx].js
+++ frontend/pages/dashboard/status/[idx].js
@@ -1,9 +1,10 @@
+/* eslint-disable import/no-default-export */
 import React from 'react';
 import { useRouter } from 'next/router';
 import { BaseDashboardLayout } from '../../../src/components/dashboard/Base';
 import { DashboardDetailedServerStatus } from '../../../src/components/dashboard/Status/detailed';
 
-export function DetailedServerStatus() {
+export default function DetailedServerStatus() {
   const router = useRouter();
   const { idx } = router.query;
 
