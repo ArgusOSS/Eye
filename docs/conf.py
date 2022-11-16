5ba92933493ade6af1006f9c1ca75b9f967b430c
0x0elliot <wolfwithahat@protonmail.com>
2022-11-17 01:02:25 +0530

    Setting up API doc

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,5 @@
+# flake8: noqa
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -8,35 +10,33 @@
 
 import sphinx_rtd_theme
 
-project = 'Eye'
-copyright = '2022, 0x0elliot, SyedAhkam'
-author = '0x0elliot, SyedAhkam'
-release = 'v0.1'
+project = "Eye"
+copyright = "2022, 0x0elliot, SyedAhkam"
+author = "0x0elliot, SyedAhkam"
+release = "v0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinxcontrib.redoc',
-    'sphinx_rtd_theme'
-]
+extensions = ["sphinxcontrib.redoc", "sphinx_rtd_theme"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 
 redoc = [
     {
-        'name': 'Eye API',
-        'page': 'api-doc',
-        'spec': 'schema.yml',
-        'embed': True,
+        "name": "Eye API",
+        "page": "api-doc",
+        "spec": "schema.yml",
+        "embed": True,
+        "opts": {"suppress-warnings": True, "hide-hostname": True},
     }
 ]
-redoc_uri = 'https://unpkg.com/redoc@2.0.0-rc.66/bundles/redoc.standalone.js'
+redoc_uri = "https://unpkg.com/redoc@2.0.0-rc.66/bundles/redoc.standalone.js"
--- docs/index.rst
+++ docs/index.rst
@@ -9,8 +9,7 @@ Welcome to Eye's documentation!
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
-
-
+   api-doc
 
 Indices and tables
 ==================
