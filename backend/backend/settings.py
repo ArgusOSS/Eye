2863c9e4ec4ba7d2f03b6296baaa00f208b55f78
0x0elliot <wolfwithahat@protonmail.com>
2022-11-17 05:46:23 +0530

    Fixes #53

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -91,7 +91,7 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     # for the time being, removing this
-    # "django.middleware.csrf.CsrfViewMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
