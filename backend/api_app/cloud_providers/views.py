cf615e84dd1bc390567293d316ac7cd08d33c77d
0x0elliot <wolfwithahat@protonmail.com>
2022-11-18 01:36:10 +0530

    Adding projects endpoint

--- backend/api_app/cloud_providers/views.py
+++ backend/api_app/cloud_providers/views.py
@@ -1,9 +1,15 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, viewsets
 
-from .serializers import CloudProviderSerializer
+from .serializers import CloudProviderSerializer, ProjectProviderSerializer
 
 
 class CloudProviderListView(generics.ListAPIView):
     serializer_class = CloudProviderSerializer
+    queryset = serializer_class.Meta.model.objects.all()
+    permission_class = [permissions.IsAuthenticated]
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    serializer_class = ProjectProviderSerializer
     queryset = serializer_class.Meta.model.objects.all()
     permission_class = [permissions.IsAuthenticated]
