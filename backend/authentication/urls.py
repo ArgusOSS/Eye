66c490f5826014ff21ee146961bb87f4916dc2eb
Abheek Tripathy <abheek.tripathy@gmail.com>
2022-11-23 14:09:36 +0530

    disabling register api endpoint

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -13,7 +13,7 @@ from .views import (
 urlpatterns = [
     path("login", LoginAPIView.as_view(), name="login"),
     path("logout", LogoutAPIView.as_view(), name="logout"),
-    path("register", RegisterView.as_view(), name="register"),
+    # path("register", RegisterView.as_view(), name="register"),
     path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
     path("google", google_login, name="oauth_google"),
     path(
