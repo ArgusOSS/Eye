974efebce1eda831607e184a917df7ae51ad245f
Syed Ahkam <smahkam57@gmail.com>
2022-12-04 01:57:59 +0530

    Fix nextjs eslint issues

--- frontend/src/components/dashboard/Status/_latencyGraph.js
+++ frontend/src/components/dashboard/Status/_latencyGraph.js
@@ -7,53 +7,63 @@ export function LatencyGraph({ title, history, date }) {
 
   const formatData = (entry) => {
     // let createdAt = parseInt(new Date(entry.created_at).getMinutes());
-    let createdAt = new Date(entry.created_at).toLocaleTimeString();
-    let latency = parseInt(entry.time_taken.split(".")[1]) / 1000000;
+    const createdAt = new Date(entry.created_at).toLocaleTimeString();
+    const latency = parseInt(entry.time_taken.split(".")[1], 10) / 1000000;
 
     return [createdAt, latency];
-  }
+  };
 
   const hasNoData = () => {
     if (history === undefined) return true;
     if (history.count === 0) return true;
 
     return false;
-  }
+  };
 
   useEffect(() => {
     if (history.results === undefined) return;
 
-    const data = [
-      ["Time", "Latency"],
-      ...history.results.map((entry) => formatData(entry)),
-    ];
-
-    setData(data);
-  }, [date, history])
+    setData([["Time", "Latency"], ...history.results.map((entry) => formatData(entry))]);
+  }, [date, history]);
 
   const options = {
     chart: {
-      title: title,
+      title,
     },
     legend: {
       position: "none",
-    }
+    },
   };
 
-  if (hasNoData()) return (
-    <Center>
-      <Text sx={(theme) => ({
-          marginTop: theme.spacing.lg,
-        })}>No data to display</Text>
-    </Center>
-  );
+  if (hasNoData())
+    return (
+      <Center>
+        <Text
+          sx={(theme) => ({
+            marginTop: theme.spacing.lg,
+          })}
+        >
+          No data to display
+        </Text>
+      </Center>
+    );
 
   return (
-    <Paper sx={(theme) => ({
-      padding: theme.spacing.lg,
-      height: "100%",
-    })}>
-      <Chart chartType="Line" width="100%" height="400px" data={data} options={options} loader={<Text>Loading..</Text>} legendToggle />
+    <Paper
+      sx={(theme) => ({
+        padding: theme.spacing.lg,
+        height: "100%",
+      })}
+    >
+      <Chart
+        chartType="Line"
+        width="100%"
+        height="400px"
+        data={data}
+        options={options}
+        loader={<Text>Loading..</Text>}
+        legendToggle
+      />
     </Paper>
   );
 }
